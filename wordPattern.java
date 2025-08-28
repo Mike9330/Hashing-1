79a0e0da3ce30f0cdad6476fda35a04acca13f24
michaeldonato <mmd1130@gmail.com>
2025-08-28 17:31:58 -0400

    Hashing-1 completed

--- wordPattern.java
+++ wordPattern.java
@@ -1,9 +1,16 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1) <-- Max value of both maps is 26
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : At first used single map approach, not realizing mapping should go both ways
+
 import java.util.*;
 
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
-        if (words.length != pattern.length()) return false;
+        if (words.length != pattern.length()){
+            return false;
+        }
 
         Map<Character, String> charMap = new HashMap<>();
         Map<String, Character> wordMap = new HashMap<>();
